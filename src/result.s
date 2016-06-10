b034ed978753a9ca5006ed10bd814ff73a90ee6d
Cyua <466764008@qq.com>
2016-06-10 22:46:02 +0800

    finish int arithmetic

--- src/result.s
+++ src/result.s
@@ -1,14 +1,32 @@
 sys_call_id = 0x80
 exit_syscall = 0x1
 
+.data
 .text
 .global _main
 		.type _main,@function
 _main:
 		pushl	%ebp
 		movl	%esp,%ebp
-		movl	6.953216e-310dx,hello
-		movl	6.953216e-310dx,hello
+		movl	$10,%eax
+		pushl	%eax
+		movl	$20,%eax
+		popl	%edx
+		addl	%edx,%eax
+		movl	%eax,intb0
+		movl	$5,%eax
+		movl	%eax,intc1
+		movl	intb0,%eax
+		pushl	%eax
+		movl	intc1,%eax
+		popl	%edx
+		imul	%edx,%eax
+		movl	%eax,inta3
+		movl	$1,%eax
+		pushl	%eax
+		movl	$2,%eax
+		popl	%edx
+		addl	%edx,%eax
 		leave
 		ret
 
@@ -18,3 +36,8 @@ _start:
 		movl $0,%ebx
 		movl $exit_syscall,%eax
 		int $sys_call_id
+
+#bss section
+		.comm	intb0,4,4
+		.comm	intc1,4,4
+		.comm	inta3,4,4
